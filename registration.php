efc7ebf1ff98becf1e02f9a2e6839b57958c52f0
viccho1 <viccho1@umbc.edu>
2018-11-11 16:40:42 -0500

    Update registration.php
    Changed name attribute for New Password and Confirm Password into pword1 and pword2
    Changed action attribute in the form element to point to new php page: registration_data.php

--- registration.php
+++ registration.php
@@ -22,13 +22,13 @@
 <div class="border">
 <h1>First, we need some information from you</h1>
 
-<form name="register" id="register" action="home.php" method="POST">
+<form name="register" id="register" action="registration_data.php" method="POST">
 <p>First Name: <input type="text" name="fname" size="25" /></p>
 <p>Last Name: <input type="text" name="lname" size="25" /></p>
 <p>Email: <input type="text" name="email" size="25" /></p>
 <p>Birthday: <input type="date" name="bday"/></p>
-<p>New Password: <input type="password" name="pword"/></p>
-<p>Confirm Password: <input type="password" name="pword"/></p>
+<p>New Password: <input type="password" name="pword1"/></p>
+<p>Confirm Password: <input type="password" name="pword2"/></p>
 <p>Screen Name: <input type="text" name="screen"/></p>
 <p>Comments: <textarea rows="10" cols="50" name="comments" id="comments">Tell us about yourself</textarea></p>
 <input type="submit" value="Submit" />
