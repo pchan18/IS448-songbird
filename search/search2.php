bbc87584a0b25258fb9696b93eb9ed66d5a47d9a
mariarobertson <44778932+mariarobertson@users.noreply.github.com>
2018-11-18 19:46:53 -0500

    Create search2.php

---
+++ search/search2.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<!-- mypage.html first lab      -->
+<html lang="en">
+<head>
+
+		<meta http-equiv="Content-Type" content = "text/html; charset=utf-8"/>
+        <title>Song Bird</title>
+		<link rel="stylesheet" type="text/css" href="home_search.css"/>
+		<link rel="stylesheet" type="text/css" href="searchbar.css"/>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
+		
+		
+</head>
+
+<body>
+
+	<div class="navBar">
+		<a href="home.php"><img id="navLogo" src="images/SONGBIRD-WHITE.png"  alt ="White version of songbird logo" width="35" height = "35"></a>
+		<button class="navBtn"><a class="nav" href="home.php">HOME</a></button>
+		<button class="navBtn"><a class="nav" href="review.php">REVIEW</a></button>
+		<button class="navBtn"><a class="nav" href="submit.php">SUBMIT</a></button>
+		<button class="currentBtn">SEARCH</button>
+		<button class="navBtn"><a class="nav" href="profile.php">PROFILE</a></button>
+		<button class="navBtn"><a class="nav" href="logout.php">LOGOUT</a></button>
+
+	</div>
+<br/><br/>
+	<div class="sideHead">
+		<h1><a href= https://swe.umbc.edu/~mrobe1/is448/project/search.php>Search</a></h1>
+	</div>
+	
+	<div >
+	
+	
+	<hr>
+	<form class="form" method ="POST" action="titleResults.php">
+	<h1> Search  </h1>
+	<h2>Seach by Title</h2>
+		<input class="search" type="text" placeholder="Title" name="title"> 
+	<br/><br/><br />
+		<button class="searchbtn" type="submit" name="submit"> Search</button>
+		<br/>
+	</form>
+	<br/>
+	<hr>
+	<form class="form" method ="POST" action="artistResults.php">
+	<h2>Seach by Artist</h2>
+		<input class="search" type="text" placeholder="Artist" name="artist">
+		<br/><br/><br />
+		<button class="searchbtn" type="submit" name="submit"> Search</button>
+		<br/>
+	</form>
+	<hr>
+	<form class="form" method ="POST" action="userResults2.php">
+	<h2>Seach by User</h2>
+		<input class="search" type="text" placeholder="User" name="user">
+		<br/><br/><br />
+		<button class="searchbtn" type="submit" name="submit"> Search</button>
+		<br/>
+
+	</form>
+	</div>
+	<br/>
+	
+
+	<div class="main">
+	<br /><br />
+		<h1>Popular Title Searches</h1>
+		<h2>Browse Popular Searches</h2>
+		<br/>
+		<a class="results" href="search_results.html" > 
+		<img  src="images/pop.jpg" alt="pop" style="width:100px; height:100;"/> <br/> Pop</a>
+		<a class="results" href="search_results.html">
+		<img  src="images/hits.jpg" alt="hits" style="width:100px; height:100;"/> <br/>Today's Hit</a>
+		<a class="results" href="search_results.html">
+		<img  src="images/country.jpg" alt="country" style="width:100px; height:100;"/> <br/> Hot Country</a>
+		<br /><br /><br /><br /><br /><br /><br /><br /><br />
+		<h1>Popular Artist Searches</h1>
+		<h2>Browse Popular Searches</h2>
+		<img class="img" src="images/chainsmokers.jpg" alt="chainsmoker" Chainsmokers />
+		<img class="img" src="images/pink.jpg" alt="pink"/>
+		<img class="img" src="images/roses.png" alt="roses"/>
+		<img class="img" src="images/coldplay.jpg" alt="coldplay"/>
+	</div>
+	
+
+</body>
+
+</html>
