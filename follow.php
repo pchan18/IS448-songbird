03091ae28b04875a7ab75c8d1d6bc1ce3235db02
Priscilla Chan <pchan3@umbc.edu>
2018-11-18 19:30:52 -0500

    made changes to follow.php

--- follow.php
+++ follow.php
@@ -27,16 +27,16 @@
 <body>
 
 	<?php
-		echo("pressed the follow button");
+		echo("pressed the follow button <br \>");
 		$db = mysqli_connect("studentdb-maria.gl.umbc.edu","mrobe1","mrobe1","mrobe1");
 
 		if (mysqli_connect_errno())	exit("Error - could not connect to MySQL");
 		
 		$constructed_query = "INSERT INTO sb_followed (followed_song,followed_artist,uname,followed_user) VALUES ('$user')";
-		
+		print($constructed_query + "<br \>");
 			#Execute query
 			$result = mysqli_query($db, $constructed_query);
-		
+			
 			#if result object is not returned, then print an error and exit the PHP program
 			if(! $result){
 				print("Error - query could not be executed");
