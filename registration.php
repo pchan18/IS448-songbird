a06f5639caed246bb1d63f09b983b730748de33f
Victor Cho <viccho1@umbc.edu>
2018-12-18 15:06:37 -0500

    added span tags for innerhtml property

--- registration.php
+++ registration.php
@@ -7,6 +7,8 @@
 	<link rel="stylesheet" type="text/css" href="songbird.css"/>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
+	<script type = "text/javascript"  src = "registration.js" ></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js"></script>
 </head>
 <body>
 	<div class="sideBar">
@@ -16,15 +18,15 @@
 <h1>First, we need some information from you</h1>
 
 <form name="register" id="register" action="registration_data.php" method="POST">
-<p>First Name: <input type="text" name="fname" style="margin-left:72px;"/></p>
-<p>Last Name: <input type="text" name="lname" style="margin-left:75px;"/></p>
-<p>Email: <input type="text" name="email" style="margin-left:115px;"/></p>
-<p>Birthday: <input type="date" name="bday" style="margin-left:70px;"/></p>
-<p>New Password: <input type="password" name="pword1" style="margin-left:46px;"/></p>
-<p>Confirm Password: <input type="password" name="pword2" style="margin-left:20px;"/></p>
-<p>Screen Name: <input type="text" name="screen" style="margin-left:57px;"/></p>
+<p>First Name: <input type="text" name="fname" style="margin-left:72px;" id="fname"/></p>
+<p>Last Name: <input type="text" name="lname" style="margin-left:75px;" id="lname"/></p>
+<p>Email: <input type="text" name="email" style="margin-left:115px;" id="email"/> <span id="msgbox2"></span></p>
+<p>Birthday: <input type="date" name="bday" style="margin-left:70px;" id="bday"/></p>
+<p>New Password: <input type="password" name="pword1" style="margin-left:46px;" id="pw1"/></p>
+<p>Confirm Password: <input type="password" name="pword2" style="margin-left:20px;" id="pw2"/></p>
+<p>Screen Name: <input type="text" name="screen" style="margin-left:57px;" id="uname"/> <span id="msgbox"></span> </p>
 <p>Bio: <textarea rows="10" cols="50" name="bio" id="bio">Tell us about yourself</textarea></p>
-<input type="submit" value="Submit" />
+<input type="submit" value="Submit" id="button"/>
 <p></p>
 </form></div>
 
