dacc231dbd9475d3917bec0ccef8ac809ec22be3
Victor Cho <viccho1@umbc.edu>
2018-12-18 12:13:42 -0500

    fixing alignment

--- registration.php
+++ registration.php
@@ -16,13 +16,13 @@
 <h1>First, we need some information from you</h1>
 
 <form name="register" id="register" action="registration_data.php" method="POST">
-<p>First Name: <input type="text" name="fname" style="margin-left:80px;"/></p>
-<p>Last Name: <input type="text" name="lname" style="margin-left:80px;"/></p>
-<p>Email: <input type="text" name="email" style="margin-left:120px;"/></p>
-<p>Birthday: <input type="date" name="bday" style="margin-left:98px;"/></p>
-<p>New Password: <input type="password" name="pword1" style="margin-left:45px;"/></p>
+<p>First Name: <input type="text" name="fname" style="margin-left:72px;"/></p>
+<p>Last Name: <input type="text" name="lname" style="margin-left:75px;"/></p>
+<p>Email: <input type="text" name="email" style="margin-left:115px;"/></p>
+<p>Birthday: <input type="date" name="bday" style="margin-left:70px;"/></p>
+<p>New Password: <input type="password" name="pword1" style="margin-left:46px;"/></p>
 <p>Confirm Password: <input type="password" name="pword2" style="margin-left:20px;"/></p>
-<p>Screen Name: <input type="text" name="screen" style="margin-left:64px;"/></p>
+<p>Screen Name: <input type="text" name="screen" style="margin-left:57px;"/></p>
 <p>Bio: <textarea rows="10" cols="50" name="bio" id="bio">Tell us about yourself</textarea></p>
 <input type="submit" value="Submit" />
 <p></p>
