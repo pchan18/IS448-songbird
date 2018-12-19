129edafb84e63ba408355239215852dd3502020a
mariarobertson <44778932+mariarobertson@users.noreply.github.com>
2018-12-18 19:14:29 -0500

    updated javascript, ajax

--- search.php
+++ search.php
@@ -1,3 +1,4 @@
+
 <?php
 //ADD your session code here
 
@@ -22,7 +23,8 @@
 		<link rel="stylesheet" type="text/css" href="searchbar.css"/>
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
-		
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js"></script>
+		<script type="text/javascript" src="search.js"> </script>
 		
 </head>
 
@@ -49,29 +51,43 @@
 	<hr>
 	<form class="form" method ="POST" action="titleResults.php">
 	<h1> Search  </h1>
-	<h2>Seach by Title</h2>
-		<input class="search" type="text" placeholder="Title" name="title"> 
+	<h2 >Seach by Title</h2>
+		<input class="search" id="title" type="text" placeholder="Title" name="title"> 
+		<span id="msgbox"></span>
 	<br/><br/><br />
+	<input style="margin-left:8%; " placeholder="Suggested Searches" id="titleHint">
+	<br/><br/>
 		<button class="searchbtn" type="submit" name="submit"> Search</button>
 		<br/>
+	</form>
+		<br/><br/><br/>
+	<hr>
+	
+	<form class="form" method ="POST" action="artistResults.php">
+	<h2>Seach by Artist</h2>
+		<input class="search" type="text" placeholder="Artist" id="artist" name="artist">
+		<span id="msgbox2"></span>
+		<br/><br/><br />
+	<input style="margin-left:8%; " placeholder="Suggested Searches" id="artistHint">
+			<br/><br/>
+		<button class="searchbtn" type="submit" name="submit"> Search</button>
+		<br/><br/>
+
+</div>
 	</form>
 	<br/>
 	<hr>
-	<form class="form" method ="POST" action="artistResults.php">
-	<h2>Seach by Artist</h2>
-		<input class="search" type="text" placeholder="Artist" name="artist">
-		<br/><br/><br />
-		<button class="searchbtn" type="submit" name="submit"> Search</button>
-		<br/>
-	</form>
-	<hr>
 	<form class="form" method ="POST" action="userResults.php">
 	<h2>Seach by User</h2>
-		<input class="search" type="text" placeholder="User" name="user">
+		<input class="search" type="text" placeholder="User" id="user"name="user">
+		<span id="msgbox3"></span>
 		<br/><br/><br />
+		<input style="margin-left:8%;" placeholder="Suggested Searches" id="userHint">
+			<br/><br/>
 		<button class="searchbtn" type="submit" name="submit"> Search</button>
 		<br/>
 
+</div>
 	</form>
 	</div>
 	<br/>
@@ -82,11 +98,11 @@
 		<h1>Popular Title Searches</h1>
 		<h2>Browse Popular Searches</h2>
 		<br/>
-		<a class="results" href="search_results.html" > 
+		<a class="results" href="justreviews.php" > 
 		<img  src="images/pop.jpg" alt="pop" style="width:100px; height:100;"/> <br/> Pop</a>
-		<a class="results" href="search_results.html">
+		<a class="results" href="justreviews.php">
 		<img  src="images/hits.jpg" alt="hits" style="width:100px; height:100;"/> <br/>Today's Hit</a>
-		<a class="results" href="search_results.html">
+		<a class="results" href="justreviews.php">
 		<img  src="images/country.jpg" alt="country" style="width:100px; height:100;"/> <br/> Hot Country</a>
 		<br /><br /><br /><br /><br /><br /><br /><br /><br />
 		<h1>Popular Artist Searches</h1>
